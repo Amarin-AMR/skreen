b3df240c9f855c150d0932ad4dcc0c5cfdcad2c9
dragonarm007 <s6230613021@phuket.psu.ac.th>
2023-10-26 02:35:23 +0700

    add provider, and make base, ex for mvvm

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:provider/provider.dart';
+
 import 'package:skreen/constants/theme.dart';
-import 'package:skreen/features/bottom_navigation/view/screen.dart';
+
+import 'package:skreen/features/bottom_navigation/viewmodel.dart';
+import 'package:skreen/features/helpcenter/screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   runApp(
-    ProviderScope(
-      child: EasyLocalization(
-        supportedLocales: const [Locale('en'), Locale('th')],
-        path: 'res/language',
-        fallbackLocale: const Locale('th'),
-        child: const MyApp(),
-      ),
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('th')],
+      path: 'res/language',
+      fallbackLocale: const Locale('th'),
+      child: const MyApp(),
     ),
   );
 }
@@ -24,14 +25,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-      themeMode: ThemeMode.system,
-      theme: ThemeClass.darkTheme,
-      darkTheme: ThemeClass.darkTheme,
-      home: const BottomNavigationBarApp(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+            create: (_) => BottomNavigationViewModel(context: context)),
+      ],
+      child: MaterialApp(
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+        themeMode: ThemeMode.system,
+        theme: ThemeClass.darkTheme,
+        darkTheme: ThemeClass.darkTheme,
+        home: const Intro(),
+      ),
     );
   }
 }
---
+++ lib/model/cardmodel/items.dart
@@ -0,0 +1,30 @@
+List items = [
+  {
+    "header": "Upload",
+    "description":
+        "Icon at the left bottom corner, You have to touch for choose image which about skin disease",
+    "image": "assets/1.png"
+  },
+  {
+    "header": "Result",
+    "description": "Touch on 'Show Result' for show result of diagnostic",
+    "image": "assets/2.png"
+  },
+  {
+    "header": "Help",
+    "description": "Help button, for show information about that disease",
+    "image": "assets/3.png"
+  },
+  {
+    "header": "Nearby",
+    "description":
+        "Nearby button, for show nearby disease that nearby with AI diagnostic",
+    "image": "assets/4.png"
+  },
+  {
+    "header": "Map",
+    "description":
+        "Touch it for show Map and it will show clinic or hospital that can cure that disease",
+    "image": "assets/5.png"
+  },
+];
