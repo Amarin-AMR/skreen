c960bf03e5e1173e42a6ad65304754e6c09f21f1
@Amarin-kongthong <Amarin.k@poontana.co.th>
2024-09-13 16:47:40 +0700

    feat: navigation

---
+++ lib/navigation/navigation_route.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/cupertino.dart';
+
+import 'navigation_constants.dart';
+
+class NavigationRoute {
+  NavigationRoute._init();
+  static final NavigationRoute _instance = NavigationRoute._init();
+  static NavigationRoute get instance => _instance;
+
+  Route<dynamic>? generateRoute(RouteSettings settings) {
+    if (settings.name == null) return null;
+    final uri = Uri.parse(settings.name!);
+    switch (uri.path) {
+      // case AppNavConstants.DEFALT:
+      //   return normalNavigate(
+      //     const SplashScreen(),
+      //     AppNavConstants.DEFALT,
+      //     settings.arguments,
+      //   );
+      // case AppNavConstants.LOGIN:
+      //   return normalNavigate(
+      //     const LogInScreen(),
+      //     AppNavConstants.LOGIN,
+      //     settings.arguments,
+      //   );
+      // case AppNavConstants.SPLASH:
+      //   return normalNavigate(
+      //     const SplashScreen(),
+      //     AppNavConstants.SPLASH,
+      //     settings.arguments,
+      //   );
+      // case AppNavConstants.DASHBOARD:
+      //   return normalNavigate(
+      //     const DashboardScreen(),
+      //     AppNavConstants.DASHBOARD,
+      //     settings.arguments,
+      //   );
+      // case AppNavConstants.SUM_WORK:
+      //   return normalNavigate(
+      //     const SumWorkScreen(),
+      //     AppNavConstants.SUM_WORK,
+      //     settings.arguments,
+      //   );
+      // case AppNavConstants.SPLASH:
+      // return normalNavigate(
+      //   const SplashScreen(),
+      //   AppNavConstants.SPLASH,
+      //   settings.arguments,
+      // );
+
+      default:
+        return CupertinoPageRoute(
+          builder: (context) => const Text("data"),
+          // const NotFoundNavigationWidget(),
+        );
+    }
+  }
+
+  CupertinoPageRoute normalNavigate(
+    Widget widget,
+    String pageName,
+    Object? arguments,
+  ) {
+    return CupertinoPageRoute(
+      builder: (context) => widget,
+      settings: RouteSettings(
+        name: pageName,
+        arguments: arguments,
+      ),
+    );
+  }
+}
