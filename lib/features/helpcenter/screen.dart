b29ec0712d787ec074e56ff24df1097e3e7d421d
@Amarin-kongthong <Amarin.k@poontana.co.th>
2024-09-13 16:47:19 +0700

    refactor: try to make helpcenter to be mvvm

--- lib/features/helpcenter/screen.dart
+++ lib/features/helpcenter/screen.dart
@@ -1,156 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:skreen/base/base_screen.dart';
 import 'package:skreen/features/bottombar/screen.dart';
 
-import 'package:skreen/model/cardmodel/items.dart';
+import 'package:skreen/features/helpcenter/viewmodel.dart';
 
-class HelpCenterScreen extends StatefulWidget {
+class HelpCenterScreen extends StatelessWidget {
   const HelpCenterScreen({super.key});
 
   @override
-  HelpCenterScreenState createState() => HelpCenterScreenState();
-}
-
-class HelpCenterScreenState extends State<HelpCenterScreen> {
-  List<Widget> slides = items
-      .map((item) => Container(
-          padding: const EdgeInsets.symmetric(horizontal: 18.0),
-          child: Column(
-            children: <Widget>[
-              // Flexible(
-              //   flex: 1,
-              //   fit: FlexFit.tight,
-              //   child: Container(
-              //     margin: const EdgeInsets.only(left: 10, right: 10),
-              //     child: Image.asset(
-              //       item['image'],
-
-              //       // alignment: Alignment.bottomCenter,
-              //     ),
-              //   ),
-              //   // child: Image.asset("assets/manual/1.png")
-              // ),
-              Flexible(
-                flex: 1,
-                fit: FlexFit.tight,
-                child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                  child: Column(
-                    children: <Widget>[
-                      Text(item['header'],
-                          style: const TextStyle(
-                              fontSize: 50.0,
-                              fontWeight: FontWeight.w300,
-                              color: Color(0XFF3F3D56),
-                              height: 2.0)),
-                      Text(
-                        item['description'],
-                        style: const TextStyle(
-                            color: Colors.grey,
-                            letterSpacing: 1.2,
-                            fontSize: 16.0,
-                            height: 1.3),
-                        textAlign: TextAlign.center,
-                      )
-                    ],
-                  ),
-                ),
-              )
-            ],
-          )))
-      .toList();
-
-  List<Widget> indicator() => List<Widget>.generate(
-        slides.length,
-        (index) => Container(
-          margin: const EdgeInsets.symmetric(horizontal: 3.0),
-          height: 10.0,
-          width: 10.0,
-          decoration: BoxDecoration(
-            color: currentPage.round() == index
-                ? const Color(0XFF256075)
-                : const Color(0XFF256075).withOpacity(0.2),
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-        ),
-      );
-
-  double currentPage = 0.0;
-  final _pageViewController = PageController();
-
-  @override
-  void initState() {
-    super.initState();
-    _pageViewController.addListener(() {
-      setState(() {
-        currentPage = _pageViewController.page!;
-      });
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: <Widget>[
-            PageView.builder(
-              controller: _pageViewController,
-              itemCount: slides.length,
-              itemBuilder: (BuildContext context, int index) {
-                return slides[index];
-              },
-            ),
-            Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  margin: const EdgeInsets.only(top: 70.0),
-                  padding: const EdgeInsets.symmetric(vertical: 40.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: indicator(),
-                  ),
-                )
-                //  ),
+    return BaseScreen<HelpCenterViewModel>(
+      viewmodel: HelpCenterViewModel(),
+      onInit: (viewmodel) {
+        viewmodel.initializePageController();
+      },
+      builder: (context, viewmodel, child) {
+        return Scaffold(
+          body: SafeArea(
+            child: Stack(
+              children: <Widget>[
+                PageView.builder(
+                  controller: viewmodel.pageViewController,
+                  itemCount: viewmodel.slides.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return viewmodel.slides[index];
+                  },
                 ),
-            // )
-            Container(
-              alignment: Alignment.bottomCenter,
-              margin: const EdgeInsets.only(bottom: 60),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 70.0),
+                    padding: const EdgeInsets.symmetric(vertical: 40.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: viewmodel.indicator(),
                     ),
-                    backgroundColor: Colors.orange),
-                child: const Text('Begin'),
-                onPressed: () async {
-                  Navigator.push(
-                    context,
-                    PageRouteBuilder(
-                      pageBuilder: (context, animation, secondaryAnimation) =>
-                          const BottomNavigationBarExample(),
-                      transitionsBuilder:
-                          (context, animation, secondaryAnimation, child) {
-                        const begin = Offset(0.0, 1.0);
-                        const end = Offset.zero;
-                        const curve = Curves.ease;
-
-                        var tween = Tween(begin: begin, end: end)
-                            .chain(CurveTween(curve: curve));
-
-                        return SlideTransition(
-                          position: animation.drive(tween),
-                          child: child,
-                        );
-                      },
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.bottomCenter,
+                  margin: const EdgeInsets.only(bottom: 60),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      backgroundColor: Colors.orange,
                     ),
-                  );
-                },
-              ),
-            )
-          ],
-        ),
-      ),
+                    child: const Text('Begin'),
+                    onPressed: () async {
+                      Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder:
+                              (context, animation, secondaryAnimation) =>
+                                  const BottomNavigationBarExample(),
+                          transitionsBuilder:
+                              (context, animation, secondaryAnimation, child) {
+                            const begin = Offset(0.0, 1.0);
+                            const end = Offset.zero;
+                            const curve = Curves.ease;
+
+                            var tween = Tween(begin: begin, end: end)
+                                .chain(CurveTween(curve: curve));
+
+                            return SlideTransition(
+                              position: animation.drive(tween),
+                              child: child,
+                            );
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/features/helpcenter/viewmodel.dart
+++ lib/features/helpcenter/viewmodel.dart
@@ -1,6 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:skreen/base/base_viewmodel.dart';
+import 'package:skreen/model/cardmodel/items.dart';
 
-class HelpCenterScreenViewModel extends BaseViewModel {
-  HelpCenterScreenViewModel({required BuildContext context});
+class HelpCenterViewModel extends BaseViewModel {
+  List<Widget> slides = [];
+  double currentPage = 0.0;
+  final PageController pageViewController = PageController();
+
+  HelpCenterViewModel() {
+    slides = items.map((item) {
+      return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 18.0),
+        child: Column(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              fit: FlexFit.tight,
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      item['header'],
+                      style: const TextStyle(
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.w300,
+                        color: Color(0XFF3F3D56),
+                        height: 2.0,
+                      ),
+                    ),
+                    Text(
+                      item['description'],
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        letterSpacing: 1.2,
+                        fontSize: 16.0,
+                        height: 1.3,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+  }
+
+  void initializePageController() {
+    pageViewController.addListener(() {
+      currentPage = pageViewController.page!;
+      notifyListeners(); // Notify listeners to rebuild when page changes
+    });
+  }
+
+  List<Widget> indicator() {
+    return List<Widget>.generate(
+      slides.length,
+      (index) => Container(
+        margin: const EdgeInsets.symmetric(horizontal: 3.0),
+        height: 10.0,
+        width: 10.0,
+        decoration: BoxDecoration(
+          color: currentPage.round() == index
+              ? const Color(0XFF256075)
+              : const Color(0XFF256075).withOpacity(0.2),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    pageViewController.dispose();
+    super.dispose();
+  }
 }
